34ade7fdfafab82c1c0db38cfdca2999734a8e12
yaseen-8547 <ycs613678@gmail.com>
2026-01-18 22:25:51 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/INSTANT-BUY-E-COMMERCE-USING-REACT/",
+  base: "/",
   server: {
     proxy: {
       '/api': {
