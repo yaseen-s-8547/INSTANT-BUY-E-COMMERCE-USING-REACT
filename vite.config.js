db569d492626625d88f8dd23e4f1137246cdcccd
yaseen-8547 <ycs613678@gmail.com>
2026-01-19 16:13:29 +0530

    fix: correct API proxy path from '/image' to '/images'

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         changeOrigin: true,
         secure:true
       },
-      '/image': {
+      '/images': {
         target: 'https://instant-buy-backend.onrender.com',
         changeOrigin: true,
         secure:true
